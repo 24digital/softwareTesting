af57d80ee22b857ab2dc5769184de8e528ef7d64
Marion <24digital@gmail.com>
2016-03-29 12:44:24 -0400

    Initial commit

---
+++ src/main/java/Test.java
@@ -0,0 +1,10 @@
+/**
+ * Created by Marion on 03/29/16.
+ */
+public class Test {
+
+    public void k(String s)
+    {
+        System.out.println(s);
+    }
+}
---
+++ src/test/java/TestTest.java
@@ -0,0 +1,14 @@
+import org.junit.*;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Marion on 03/29/16.
+ */
+public class TestTest {
+
+    @org.junit.Test
+    public void k() throws Exception {
+
+    }
+}
